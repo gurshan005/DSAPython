2afd73335a3f8b7028b617b21321dd41d443f8a8
Shan Bhogal <102265978+gurshan005@users.noreply.github.com>
2024-09-11 15:31:27 -0400

    Add files via upload

---
+++ bhog2062_l10 3/bhog2062_datastructures/src/Stack_array.py
@@ -0,0 +1,158 @@
+"""
+-------------------------------------------------------
+Array version of the Stack ADT.
+-------------------------------------------------------
+Author:  David Brown
+ID:      123456789
+Email:   dbrown@wlu.ca
+__updated__ = "2024-01-15"
+-------------------------------------------------------
+"""
+# Imports
+from copy import deepcopy
+
+
+class Stack:
+
+    def __init__(self):
+        """
+        -------------------------------------------------------
+        Initializes an empty stack. Data is stored in a Python list.
+        Use: s = Stack()
+        -------------------------------------------------------
+        Returns:
+            a new Stack object (Stack)
+        -------------------------------------------------------
+        """
+        self._values = []
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the stack is empty.
+        Use: b = s.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if the stack is empty, False otherwise
+        -------------------------------------------------------
+        """
+
+        # Your code here
+        if len(self._values) == 0: 
+            is_empty = True 
+        else: 
+            is_empty = False
+        
+        return is_empty
+    
+        
+
+    def push(self, value):
+        """
+        -------------------------------------------------------
+        Pushes a copy of value onto the top of the stack.
+        Use: s.push(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a data element (?)
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+
+        # Your code here
+        self._values.append(deepcopy(value))
+        
+        
+
+    def pop(self):
+        """
+        -------------------------------------------------------
+        Pops and returns the top of stack. The value is removed
+        from the stack. Attempting to pop from an empty stack
+        throws an exception.
+        Use: value = s.pop()
+        -------------------------------------------------------
+        Returns:
+            value - the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot pop from an empty stack"
+
+        # Your code here
+        value = self._values.pop()
+        
+        return value
+
+    def peek(self):
+        """
+        -------------------------------------------------------
+        Returns a copy of the value at the top of the stack.
+        Attempting to peek at an empty stack throws an exception.
+        Use: value = s.peek()
+        -------------------------------------------------------
+        Returns:
+            value - a copy of the value at the top of the stack (?)
+        -------------------------------------------------------
+        """
+        assert len(self._values) > 0, "Cannot peek at an empty stack"
+
+        # Your code here
+        return deepcopy(self._values[-1])
+
+
+    def __iter__(self):
+        """
+        FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the stack
+        from top to bottom.
+        Use: for v in s:
+        -------------------------------------------------------
+        Returns:
+            value - the next value in the stack (?)
+        -------------------------------------------------------
+        """
+        for value in self._values[::-1]:
+            yield value
+            
+            
+            
+    def split_alt(self):
+        """
+        -------------------------------------------------------
+        Splits the source stack into separate target stacks with values
+        alternating into the targets. At finish source stack is empty.
+        Use: target1, target2 = source.split_alt()
+        -------------------------------------------------------
+        Returns:
+            target1 - contains alternating values from source (Stack)
+            target2 - contains other alternating values from source (Stack)
+        -------------------------------------------------------
+        """
+        target1 = Stack()
+        target2 = Stack()
+        toggle = True
+
+        while self._values:
+            if toggle:
+                target1._values.append(self._values.pop())
+            else:
+                target2._values.append(self._values.pop())
+            toggle = not toggle
+
+        return target1, target2
+
+    def reverse(self):
+        """
+        -------------------------------------------------------
+        Reverses the contents of the source stack.
+        Use: source.reverse()
+        -------------------------------------------------------
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        self._values = self._values[::-1]
+
+        return None
